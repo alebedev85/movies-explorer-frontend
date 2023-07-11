b4ffc33f6360d5873db1062aff635c696b50d828
Andrey Lebedev <alebedev424@gmail.com>
2023-07-11 11:45:10 +0300

    refactor: сделал стили для Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,28 +6,28 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="footer">
-      <p className="footer__title">
+    <footer className='footer'>
+      <p className='footer__title'>
         Учебный проект Яндекс.Практикум х BeatFilm.
       </p>
-      <div className="footer__navigation">
-        <p className="footer__copyright">
+      <div className='footer__navigation'>
+        <p className='footer__links__text'>
           &copy; {currentYear}
         </p>
-        <ul className="footer__links">
-          <li className='footer__link'>
-            <a href="https://practicum.yandex.ru"
-              className="footer__link link"
-              rel="noreferrer"
-              target="_blank">
+        <ul className='footer__links'>
+          <li>
+            <a href='https://practicum.yandex.ru'
+              className='footer__links__text link'
+              rel='noreferrer'
+              target='_blank'>
               Яндекс.Практикум
             </a>
           </li>
-          <li className='footer__link'>
-            <a href="https://github.com/alebedev85/movies-explorer-frontend"
-              className="footer__link link"
-              rel="noreferrer"
-              target="_blank">
+          <li>
+            <a href='https://github.com/alebedev85/movies-explorer-frontend'
+              className='footer__links__text link'
+              rel='noreferrer'
+              target='_blank'>
               Github
             </a>
           </li>
