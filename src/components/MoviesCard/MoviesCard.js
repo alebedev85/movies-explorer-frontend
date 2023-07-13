400c9777f4cd1ce9bb235ed873effaa2cc0d0e14
Andrey Lebedev <alebedev424@gmail.com>
2023-07-13 12:09:02 +0300

    refactor: сделал разметку для карточек

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -4,20 +4,23 @@ import './MoviesCard.css';
 function MoviesCard({ card }) {
   return (
     <li className="card" >
-      <a href={card.trailerLink} className='card__link link' target="_blank" rel="noreferrer">
-        <img className='card__poster' src={card.image} alt='Постер к фильму' />
-      </a>
-      <h2 className='card__name'>
-        {card.nameRU}
-      </h2>
-      <p className='card__duration'>
-        {Math.floor(card.duration/60)}ч {card.duration - 60 * Math.floor(card.duration/60)}м
-      </p>
-      <button className='card__button
+      <div className='card_info'>
+        <h2 className='card__name'>
+          {card.nameRU}
+        </h2>
+        <p className='card__duration'>
+          {Math.floor(card.duration / 60)}ч {card.duration - 60 * Math.floor(card.duration / 60)}м
+        </p>
+        <button className='card__button
         card__button_save
         card__button_save-active
         button'
-        type='button' />
+          type='button' />
+      </div>
+      <a href={card.trailerLink} className='card__link link' target="_blank" rel="noreferrer">
+        <img className='card__poster' src={card.image} alt='Постер к фильму' />
+      </a>
+
     </li>
   );
 }
