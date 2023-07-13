18443e7a8cdfaf589da56cff347098b6e96dca22
Andrey Lebedev <alebedev424@gmail.com>
2023-07-13 13:52:18 +0300

    refactor: добавил роутинга кнопкам

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './MoviesCard.css';
 
 function MoviesCard({ card, isSaved }) {
+
+  const location = useLocation();
+
   return (
     <li className="card" >
       <div className='card_info'>
@@ -11,17 +15,18 @@ function MoviesCard({ card, isSaved }) {
         <p className='card__duration'>
           {Math.floor(card.duration / 60)}ч {card.duration - 60 * Math.floor(card.duration / 60)}м
         </p>
-        {/* <button className='card__button
-        card__button_save
-        card__button_save-active
-        button'
-          type='button' /> */}
-          <button className={`
-          card__button
-          button
-          ${isSaved ? 'card__button_save-active' : 'card__button_save'}
-          `}
-          type='button' />
+        {location.pathname === "/movies" &&
+          <button className={
+            `card__button
+                button
+                ${isSaved ? 'card__button_save-active' : 'card__button_save'}`
+          }
+            type='button' />
+        }
+        {location.pathname === "/saved-movies" &&
+          <button className='card__button button card__button__delete'
+            type='button' />
+        }
       </div>
       <a href={card.trailerLink} className='card__link link' target="_blank" rel="noreferrer">
         <img className='card__poster' src={card.image} alt='Постер к фильму' />
