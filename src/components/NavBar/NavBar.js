c9e1372754792a9270265e0741a22c552fe43820
Andrey Lebedev <alebedev424@gmail.com>
2023-07-17 15:47:59 +0300

    feater: сделал sidebar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,6 +8,16 @@ function NavBar({ loggedIn, onClick }) {
 
   const location = useLocation();
 
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
+
+  function openSidebar() {
+    setIsSidebarOpen(true)
+  }
+
+  function closeSidebar() {
+    setIsSidebarOpen(false)
+  }
+
   return !loggedIn ? (
     <nav className="navbar">
       <NavLink to="/#" className="navbar__link link">Регистрация</NavLink>
@@ -39,8 +49,6 @@ function NavBar({ loggedIn, onClick }) {
           </NavLink>
         </li>
       </ul>
-
-
       <NavLink to="/#" className="navbar__account-button link">
         <p className='navbar__link navbar__link__size_14'>
           Аккаунт
@@ -50,6 +58,61 @@ function NavBar({ loggedIn, onClick }) {
           src={accountIcon}
           alt='Иконка входа в аккаунт' />
       </NavLink>
+
+      {/* Выпадающее меню */}
+      <aside className='sidebar sidebar_opened'>
+        <button
+          className='sidebar__close-button'
+          type='button'
+          onClick={closeSidebar}
+        />
+        <div className='sidebar__container'>
+          <ul className='navbar-links navbar-links_sidebar'>
+            <li>
+              <NavLink
+                to="/movies"
+                className={`navbar__link
+                    navbar__link__size_18
+                    link`
+                }>
+                Главная
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/movies"
+                className={`navbar__link
+                    navbar__link__size_18
+                    link
+                    ${location.pathname === "/movies" && 'navbar__link_active'}`
+                }>
+                Фильмы
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/saved-movies"
+                className={`navbar__link
+                    navbar__link__size_18
+                    link
+                    ${location.pathname === "/saved-movies" && 'navbar__link_active'}`
+                }>
+                Сохранённые фильмы
+              </NavLink>
+            </li>
+          </ul>
+          <NavLink to="/#" className="navbar__account-button link">
+            <p className='navbar__link navbar__link__size_14'>
+              Аккаунт
+            </p>
+            <img
+              className='navbar__account-button__icon '
+              src={accountIcon}
+              alt='Иконка входа в аккаунт' />
+          </NavLink>
+        </div>
+      </aside>
+
     </nav>
   );
 }
