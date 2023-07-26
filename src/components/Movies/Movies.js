6dd1050c8c4c81c94241997d2fe4ad3623c1fcd5
Andrey Lebedev <alebedev424@gmail.com>
2023-07-26 10:44:18 +0300

    refactor: ренейминг в Movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -13,18 +13,18 @@ function Movies() {
 
   const [cardsNumber, setCardsNumber] = useState({ first: 12, next: 3, }); //стейт для колличества карточек на экране
   const [isPreloader, setIsPreloader] = useState(false); //стейт состояния прелоудора
-  const [cards, setCards] = useState([]); //стейт для всех карточек
+  const [beatfilmMmovies, setBeatfilmMmovies] = useState([]); //стейт для всех карточек
   const [displayedCards, setDisplayedCards] = useState(cardsNumber.first); //стейт для показываемых карточек
 
   //проверка localStorage и получение карточек
   useEffect(() => {
-    setCards(JSON.parse(localStorage.getItem('cards')));
-    if (!cards.length) {
+    setBeatfilmMmovies(JSON.parse(localStorage.getItem('beatfilmMmovies')));
+    if (!beatfilmMmovies.length) {
       setIsPreloader(true);
       moviesApi.getCards()
         .then((res) => {
-          setCards(res);
-          localStorage.setItem('cards', JSON.stringify(res));
+          setBeatfilmMmovies(res);
+          localStorage.setItem('beatfilmMmovies', JSON.stringify(res));
         })
         .catch((err) => console.log(err))
         .finally(setIsPreloader(false));
@@ -61,27 +61,21 @@ function Movies() {
     setDisplayedCards(displayedCards + cardsNumber.next)
   };
 
-  function func(text) {
-    cards.forEach(item => console.log(Object.values(item)
-    .some(value => typeof value === "string" ? value.includes(text) : value === text)))
-    // return cards.filter(item => Object.values(item).includes(text))
-  }
-
   //обработчик поиска фильмов
   function handleSearchMovie(text) {
-    // console.log(cards.filter(movie => Object.values(movie[0]).includes(text)));
-    console.log(cards.filter(card => Object.values(card)
-    .some(value => typeof value === "string" ? value.includes(text) : value === text)))
-    // func(text)
+    const searchResalt = beatfilmMmovies.filter(card => Object.values(card)
+      .some(value => typeof value === "string" ? value.includes(text) : value === text))
+    console.log(searchResalt)
+
   };
 
   return (
     <main className="main">
       <SearchForm onSearchMovie={handleSearchMovie} />
-      {cards ? <MoviesCardList
-        cards={cards.slice(0, displayedCards)}
+      {beatfilmMmovies ? <MoviesCardList
+        cards={beatfilmMmovies.slice(0, displayedCards)}
         onClick={handleNextCards}
-        buttonVisibility={cards.length > displayedCards}
+        buttonVisibility={beatfilmMmovies.length > displayedCards}
       />
         :
         <Preloader />}
