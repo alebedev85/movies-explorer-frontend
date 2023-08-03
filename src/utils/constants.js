fcda4e5a86ca5317623249a812ffe3dec537c68b
Andrey Lebedev <alebedev424@gmail.com>
2023-08-03 11:49:45 +0300

    refactor: добавил https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const beatfilm_URL = 'https://api.nomoreparties.co/beatfilm-movies';
-export const BASE_URL = 'http://api.andrey.movies.nomoreparties.sbs';
+export const BASE_URL = 'https://api.andrey.movies.nomoreparties.sbs';
 
 
 export const SCREEN_S = 320;
